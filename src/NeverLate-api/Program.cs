7906006bf85506f74257f367acd22a9b4e79a670
klobacz <kuba.lobacz@gmail.com>
2022-05-18 01:26:41 +0200

    Setup webapi project

---
+++ src/NeverLate-api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using NeverLate_api.Persistence.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NeverLateContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("NeverLateDB");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
