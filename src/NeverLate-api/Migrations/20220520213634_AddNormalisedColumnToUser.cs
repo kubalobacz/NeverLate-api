0bbe295016c7b2d384326712cf131eba5c432790
klobacz <kuba.lobacz@gmail.com>
2022-05-20 23:40:45 +0200

    Add migrations

---
+++ src/NeverLate-api/Migrations/20220520213634_AddNormalisedColumnToUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeverLate_api.Migrations
+{
+    public partial class AddNormalisedColumnToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedEmail",
+                table: "AspNetUsers",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedEmail",
+                table: "AspNetUsers");
+        }
+    }
+}
