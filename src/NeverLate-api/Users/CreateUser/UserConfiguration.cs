731b5f75d0dce798acad44319fd1a0215300e3f5
klobacz <kuba.lobacz@gmail.com>
2022-06-07 23:15:45 +0200

    Create user table seeding configuration.

---
+++ src/NeverLate-api/Users/CreateUser/UserConfiguration.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NeverLate_api.Users.CreateUser;
+
+public class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+{
+    public void Configure(EntityTypeBuilder<IdentityUser> builder)
+    {
+        var passwordHasher = new PasswordHasher<IdentityUser>();
+        var usersArray = new IdentityUser[]
+        {
+            new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "test_user_1",
+                Email = "test_user_1@test.com",
+                NormalizedEmail = "TEST_USER_1@TEST.COM",
+                PasswordHash = passwordHasher.HashPassword(null, "password123")
+            }
+        };
+        builder.HasData(usersArray);
+    }
+}
