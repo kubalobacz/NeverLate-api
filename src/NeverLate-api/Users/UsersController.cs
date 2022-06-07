37670441877d3f756c7c035cf87cebf2cd19b040
klobacz <kuba.lobacz@gmail.com>
2022-06-07 23:10:16 +0200

    Move UsersController to users folder.

---
+++ src/NeverLate-api/Users/UsersController.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NeverLate_api.Users.CreateUser;
+
+namespace NeverLate_api.Users;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(CreateUserRequest request)
+    {
+        var result = await _mediator.Send(new CreateUserCommand(request.Email, request.Password));
+
+        return result switch
+        {
+            {ErrorReason: CreateUserErrorReasonEnum.UserWithSameEmailFound} => BadRequest(),
+            {IsSuccess: false} => StatusCode(500, "Creating user failed."),
+            _ => Ok("User created.")
+        };
+    }
+}
